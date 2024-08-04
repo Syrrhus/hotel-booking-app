46c3ec54735a9356866cf64caccf3f7b019daf98
Shrotofo <109517968+shrotofo@users.noreply.github.com>
2024-08-04 13:30:29 +0800

    Delete hotelBooking-jameslatest/hotelBooking-james2 directory

--- hotelBooking-jameslatest/hotelBooking-james2/Frontend/jest.config.js
+++
@@ -1,8 +0,0 @@
-// jest.config.js
-module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-  moduleNameMapper: {
-    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js'
-  },
-  testEnvironment: 'jsdom'
-};
