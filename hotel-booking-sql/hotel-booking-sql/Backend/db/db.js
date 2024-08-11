bc7afae3c372dff6e1689ca469fed56a9622a2a2
janyaa07 <140350660+janyaa07@users.noreply.github.com>
2024-08-11 19:18:37 +0800

    added changes

--- hotel-booking-sql/hotel-booking-sql/Backend/db/db.js
+++ hotel-booking-sql/hotel-booking-sql/Backend/db/db.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2';
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'password',
+  password: 'hello@0706',
   database: 'booking_system'
 });
 
